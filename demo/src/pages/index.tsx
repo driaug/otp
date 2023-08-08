48077ccecfefc6d46de7a45f8a4e56385dae8140
Dries Augustyns <inbox@driaug.com>
2023-08-08 19:40:14 +0200

    cleanup

--- demo/src/pages/index.tsx
+++ demo/src/pages/index.tsx
@@ -9,15 +9,13 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-center bg-spring-wood-100 space-y-9 ${lora.className}`}
     >
       <div className='text-center'>
-      <h1 className='font-semibold text-4xl text-neutral-800'>
-        One-Time Password
-      </h1>
-      <p className='font-medium text-neutral-500 max-w-lg'>
-        OTP component for React. Built with Tailwind. Types included.
-      </p>
+        <h1 className='font-semibold text-4xl text-neutral-800'>
+          One-Time Password
+        </h1>
+        <p className='font-medium text-neutral-500 max-w-lg mt-1'>
+          OTP component for React. Built with Tailwind. Types included.
+        </p>
       </div>
-
-
 
       <OTP onComplete={(otp) => console.log(otp)} />
     </main>
